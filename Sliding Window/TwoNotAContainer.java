7fcb9af7c3c863dadb4abc6f9dcb6fef1171541f
avaneeshshetye <Avaneesh1305@gmail.com>
2024-04-04 21:41:57 +0530

    kk

---
+++ Sliding Window/TwoNotAContainer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TwoNotAContainer {
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+        int num; // No need to initialize num here
+        
+        do { // Loop continues until a positive number is entered
+            System.out.print("Enter a positive number: ");
+            num = scanner.nextInt();
+            if (num <= 0) {
+                System.out.println("Invalid input. Please enter a positive number.");
+            }
+        } while (num <= 0);
+        
+        System.out.println("You entered: " + num);
+        scanner.close();
+    }
+    }
